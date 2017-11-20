f2e080feaa2a30ec4563903c551063f52baf1d72
hexiaosong <he20093334@163.com>
2017-11-20 15:43:09 +0800

    chg: 微信公众号爬虫定时任务上传至hive.

--- scheduler/WxGongZhongHao.py
+++ scheduler/WxGongZhongHao.py
@@ -10,18 +10,20 @@ from __future__ import  unicode_literals
 from subprocess import Popen
 from apscheduler.schedulers.blocking import BlockingScheduler
 from utils.common import yesterday_date
+from spiders import PROJECT_ROOT
 
 
 sched = BlockingScheduler()
 
 
-# @sched.scheduled_job('cron', day_of_week='1-7', hour=13, minute=10, second=0)
-@sched.scheduled_job('interval', hours=6)
+# @sched.scheduled_job('interval', hours=6)
+@sched.scheduled_job('interval', minutes=1)
 def cron_upload_wx_spider():
     date = yesterday_date()
+    print 'yesterday is %s' % date
+    file_handle = open("{}/log/upload_wx_hive.log".format(PROJECT_ROOT), 'w+')
     shell_cmd = 'fab winxin2hive:{}'.format(date)
-    Popen(shell_cmd, shell=True)
-    # Popen.wait()
+    Popen(shell_cmd, shell=True, stdout=file_handle)
 
 if __name__ == '__main__':
     print 'starting upload winxin_data to hive...'
